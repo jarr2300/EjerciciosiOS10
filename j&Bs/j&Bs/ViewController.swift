e27717e4d3b876dd6b4058453b9b0f3cecb2712f
jarr2300 <jarr2300foros@gmail.com>
2019-01-26 16:07:31 +0100

    Diseño base

---
+++ j&Bs/j&Bs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  j&Bs
+//
+//  Created by Macabuku on 26/1/19.
+//  Copyright © 2019 Macabuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
